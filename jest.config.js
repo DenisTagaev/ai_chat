9526e43ef4a5f449c5d8e061bdc383b3e55f2e10
Denis Tagaev <sivean.27@gmail.com>
2025-11-17 01:01:02 -0500

    Tests packages+setup

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+const { createDefaultPreset } = require("ts-jest");
+
+const tsJestTransformCfg = createDefaultPreset().transform;
+
+/** @type {import("jest").Config} **/
+export default {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  roots: ["<rootDir>/src/tests"],
+  testMatch: ["**/*.test.ts"],
+  collectCoverage: true,
+  coverageDirectory: "<rootDir>/coverage",
+  transform: {
+    ...tsJestTransformCfg,
+  },
+};
