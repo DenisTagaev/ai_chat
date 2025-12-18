77ffab527c5b2a7d965ae0bf14301c1da4d4b938
Denis Tagaev <sivean.27@gmail.com>
2025-12-17 23:59:46 -0500

    eader component init

---
+++ ui/src/components/Header.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+    import { useUserStore } from '../stores/user';
+    import { useRouter } from 'vue-router';
+    import chatImage from '../assets/pngtree-chatbot-messenger-concept-design-man-and-woman-chatting-using-chatbots-assistant-png-image_3829211-removebg-preview.png';
+
+    const userStore = useUserStore();
+    const router = useRouter();
+</script>
+
+<template>
+    <section class="py-4 px-6 bg-slate-800 shadowm-md flex justify-between">
+        <img
+            :src="chatImage"
+            class="w-8 h-8"
+            alt="AI bot image"
+            loading="lazy"
+            decoding="async"
+        />
+        <h1 class="text-lg font-semibold">AI Assistant</h1>
+        <button @click="logout" class="text-slate-400 hover:text-slate-100">Logout</button>
+    </section>
+</template>
--- ui/src/views/ChatView.vue
+++ ui/src/views/ChatView.vue
@@ -1,3 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+    import Header from '../components/Header.vue';
+</script>
 
-<template></template>
+<template>
+    <section class="flex flex-col h-screen bg-slate-900 text-slate-50">
+        <Header></Header>
+    </section>
+</template>
