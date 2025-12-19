e9db6df90d0b08b4fefa243f7d3d0092647f45e9
Denis Tagaev <sivean.27@gmail.com>
2025-12-19 01:29:44 -0500

    update theming to rely on os settings

--- ui/src/components/Header.vue
+++ ui/src/components/Header.vue
@@ -1,14 +1,11 @@
 <script setup lang="ts">
     import { useUserStore } from '../stores/user';
     import { useRouter } from 'vue-router';
-    import { useDark, useToggle } from '@vueuse/core';
-    import { Sun, Moon, LogOutIcon } from 'lucide-vue-next';
+    import { LogOutIcon } from 'lucide-vue-next';
     import chatImage from '../assets/pngtree-chatbot-messenger-concept-design-man-and-woman-chatting-using-chatbots-assistant-png-image_3829211-removebg-preview.png';
 
     const userStore = useUserStore();
     const router = useRouter();
-    const isDark = useDark();
-    const toggleTheme = useToggle(isDark);
 
     const logout = () => {
         userStore.logout();
@@ -17,7 +14,7 @@
 </script>
 
 <template>
-    <nav class="py-4 px-6 bg-slate-100 dark:bg-slate-800 shadowm-md flex items-center justify-between" role="banner">
+    <nav class="py-4 px-6 bg-slate-100 dark:bg-slate-800 shadow-md flex items-center justify-between" role="banner">
         <div class="flex items-center gap-2">
             <img
                 :src="chatImage"
@@ -29,15 +26,6 @@
             <h1 class="text-lg font-semibold dark:text-slate-100">AI Assistant</h1>
         </div>
         <div class="flex items-center gap-3">
-            <button
-                @click="toggleTheme()"
-                class="p-2 rounded-md text-slate-300 hover:bg-slate-700 hover:text-slate-100 dark:hover:bg-slate-100 dark:hover:text-slate-700 transition"
-                aria-label="Toggle theme"
-            >
-                <Sun v-if="!isDark" class="w-5 h-5" />
-                <Moon v-else class="w-5 h-5" />
-            </button>
-
             <button
                 @click="logout"
                 class="flex items-center gap-2 text-slate-300 hover:text-rose-400 transition font-medium"
