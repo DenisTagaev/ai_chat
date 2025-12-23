441795c3583d82091575b738b509bdb407cf86db
Denis Tagaev <sivean.27@gmail.com>
2025-12-23 13:47:16 -0500

    Chat store AI request function, file cleanup

--- ui/src/stores/chat.ts
+++ ui/src/stores/chat.ts
@@ -15,19 +15,20 @@ interface FormattedMessageState {
 
 export const useChatStore = defineStore("chat",  () => {
     const messages = ref<FormattedMessageState[]>([]);
-    const loading = ref(false);
+    const isLoading = ref(false);
     const error = ref<string | null>(null);
     const userStore = useUserStore();
+    const baseURL: string = import.meta.env.VITE_API_URL + 'api/ai';
 
-    const loadChatHistory = async () => {
+    const loadChatHistory = async (): Promise<void> => {
         if(!userStore.userId) return;
 
-        loading.value = true;
+        isLoading.value = true;
         error.value = null;
 
         try {
             const { data } = await axios.post(
-              `${import.meta.env.VITE_API_URL}/api/ai/chat-history`,
+              `${baseURL}/chat-history`,
               {
                 userId: userStore.userId,
               }
@@ -42,7 +43,7 @@ export const useChatStore = defineStore("chat",  () => {
             error.value = 'Failed to load chat history';
             console.error(`Error loading chat history: ${err}`);
         } finally {
-            loading.value = false;
+            isLoading.value = false;
         }
     }
     const reset = (): void => {
@@ -50,11 +51,37 @@ export const useChatStore = defineStore("chat",  () => {
         error.value = null;
     }
 
+    const sendAIRequest = async (message: string): Promise<void> => {
+        if(!message.trim() || !userStore.userId) return;
+
+        messages.value.push({ role: 'user', content: message });
+        isLoading.value = true;
+
+        try {
+            const { data } = await axios.post(`${baseURL}/chat`, {
+                message,
+                userId: userStore.userId
+            });
+
+            messages.value.push({ role: "model", content: data.reply });
+        } catch (err) {
+            console.error('Error sending message: ', err);
+            error.value = 'Failed to reach the server';
+            messages.value.push({
+                role: 'model',
+                content: 'Error, enable to process the request'
+            });
+        } finally {
+            isLoading.value = false;
+        }
+    }
+
     return {
        messages: readonly(messages),
-       loading: readonly(loading),
+       isLoading: readonly(isLoading),
        error: readonly(error),
        loadChatHistory,
+       sendAIRequest,
        reset,
      };
 });
