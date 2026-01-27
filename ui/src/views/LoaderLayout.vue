889f03cbf1b47c9b4d2b88de1aefb65627802b4a
Denis Tagaev <sivean.27@gmail.com>
2026-01-26 23:59:09 -0500

    loader between routes intro

---
+++ ui/src/views/LoaderLayout.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+    import Loader from '../components/Loader.vue'
+</script>
+
+<template>
+  <main class="min-h-screen flex items-center justify-center bg-gray-800">
+    <Loader
+      :show="true"
+      overlay
+      label="Loading…"
+    />
+  </main>
+</template>
