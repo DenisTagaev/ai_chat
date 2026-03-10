a8344afa67b8794148764d9a573657e27018756d
Denis Tagaev <sivean.27@gmail.com>
2026-03-10 00:07:55 -0400

    Streamchat sevice typization add on

--- src/services/streamChatService.ts
+++ src/services/streamChatService.ts
@@ -1,9 +1,9 @@
-import { StreamChat, APIResponse, UserResponse, Channel } from "stream-chat";
+import { StreamChat, APIResponse, UserResponse, Channel, SendMessageAPIResponse } from "stream-chat";
 import { StreamUser } from "../utils/interfaces";
 
 export class StreamChatService {
   private static readonly streamClient: StreamChat = (() => {
-    if(!process.env.STREAM_API_KEY || !process.env.STREAM_API_SECRET) {
+    if (!process.env.STREAM_API_KEY || !process.env.STREAM_API_SECRET) {
       throw new Error("Stream API credentials are not defined");
     }
 
@@ -15,13 +15,15 @@ export class StreamChatService {
 
   /** Check if a Stream Chat user exists */
   static async checkRegisteredStreamUser(
-    id: string
+    id: string,
   ): Promise<APIResponse & { users: UserResponse[] }> {
     return await this.streamClient.queryUsers({ id: { $eq: id } });
   }
 
   /** Create or update a Stream Chat user */
-  static async createStreamUser(user: StreamUser) {
+  static async createStreamUser(
+    user: StreamUser,
+  ): Promise<APIResponse & { users: { [key: string]: UserResponse } }> {
     return await this.streamClient.upsertUser(user as UserResponse);
   }
 
@@ -38,7 +40,7 @@ export class StreamChatService {
       {
         members: [userId],
         created_by_id: "ai_bot",
-      }
+      },
     );
     await channel.watch();
     return channel;
@@ -49,17 +51,18 @@ export class StreamChatService {
   }
 
   /** Send an AI message to the user's channel */
-  static async sendMessageToAi(channel: Channel, message: string) {
-    if(!message.trim()) {
+  static async sendMessageToAi(
+    channel: Channel,
+    message: string,
+  ): Promise<SendMessageAPIResponse> {
+    if (!message.trim()) {
       throw new Error("AI bot error");
     }
 
-    return await channel.sendMessage(
-      {
-        text: message,
-        user_id: "ai_bot"
-      }
-    );
+    return await channel.sendMessage({
+      text: message,
+      user_id: "ai_bot",
+    });
   }
 
   static _getClient(): StreamChat {
