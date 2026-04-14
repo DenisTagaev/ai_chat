484098a096e7d2541c82cf54d07a63a0b2a3348c
Denis Tagaev <sivean.27@gmail.com>
2026-04-13 23:59:21 -0400

    Loki logs aggregation prep setup

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -4,6 +4,7 @@ import { RequestContext } from "./types";
 
 const isDevEnv: boolean = process.env.NODE_ENV === "development";
 const isTestEnv: boolean = process.env.NODE_ENV === "test";
+const isProdEnv: boolean = process.env.NODE_ENV === "production";
 
 function getLogEnv(): string {
     if(isTestEnv) return "silent";
@@ -14,26 +15,58 @@ function getLogEnv(): string {
 export const logger: pino.Logger<never, boolean> = pino({
   level: getLogEnv(),
 
+  formatters: {
+    level(label) {
+      return { level: label };
+    },
+  },
+
+  timestamp: pino.stdTimeFunctions.isoTime,
+
   mixin() {
     const context: RequestContext | undefined = getRequestContext();
 
-    if(!context) return {};
+    if (!context) return {};
 
     return {
       reqId: context.reqId,
       userId: context.userId,
-    }
+    };
   },
 
   ...(isDevEnv && !isTestEnv
     ? {
-      transport: {
-        target: "pino-pretty",
-        options: {
-          colorize: true,
-          translateTime: "HH:MM:ss",
-          ignore: "pid,hostname",
+        transport: {
+          target: "pino-pretty",
+          options: {
+            colorize: true,
+            translateTime: "HH:MM:ss",
+            ignore: "pid,hostname",
+          },
         },
+      }
+    : {}),
+
+  transport: isProdEnv
+    ? {
+        target: "pino-loki",
+        options: {
+          host: process.env.LOKI_HOST,
+          basicAuth: {
+            username: process.env.LOKI_USER,
+            password: process.env.LOKI_API_KEY,
+          },
+
+          labels: {
+            app: "my-backend",
+            env: process.env.NODE_ENV,
+          },
+
+          batching: true,
+          interval: 5,
+        },
+      }
+    : {
+        target: "pino-pretty",
       },
-    } : {}),
 });
