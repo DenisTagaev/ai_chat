89c6a65fbee3b7d2b56edb568705e1749066662c
Denis Tagaev <sivean.27@gmail.com>
2026-08-15 23:58:26 -0400

    Login button redesign

--- ui/src/components/auth/AuthButton.vue
+++ ui/src/components/auth/AuthButton.vue
@@ -7,10 +7,11 @@
 <template>
   <button
     type="submit"
-    class="w-full p-2 bg-sky-600 rounded-lg font-medium transition-opacity disabled:opacity-60"
+    class="w-full mt-6 px-5 py-3 bg-sky-600 border border-sky-500/50 rounded-lg font-mono font-semibold uppercase tracking-widest transition hover:bg-sky-500 hover:shadow-md hover:shadow-sky-500/20 hover:text-slate-200 disabled:opacity-50"
     :disabled="loading"
     :aria-busy="loading"
   >
+    <OhVueIcon name="bi-robot" class="mr-2"/>
     <slot />
   </button>
 </template>
