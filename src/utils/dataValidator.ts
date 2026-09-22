6e0f0e1406c97db98927ba24fc1e3cfd6a0c265c
Denis Tagaev <sivean.27@gmail.com>
2026-09-21 23:59:16 -0400

    Library utils optimization

--- src/utils/dataValidator.ts
+++ src/utils/dataValidator.ts
@@ -1,9 +1,12 @@
-import validator from "validator";
+import contains from "validator/es/lib/contains";
+import isEmail from "validator/es/lib/isEmail";
+import matches from "validator/es/lib/matches";
+import normalizeEmail from "validator/es/lib/normalizeEmail";
 
 export function validateAndNormalizeData(
   name: string,
   email: string,
-): { email: string }  | { error: string } {
+): { email: string } | { error: string } {
   if (
     !name ||
     typeof name !== "string" ||
@@ -16,23 +19,23 @@ export function validateAndNormalizeData(
   const normalizedName: string = name.trim();
 
   if (
-    validator.contains(normalizedName, "<") ||
-    validator.contains(normalizedName, ">") ||
-    validator.contains(normalizedName, "{") ||
-    validator.contains(normalizedName, "}")
+    contains(normalizedName, "<") ||
+    contains(normalizedName, ">") ||
+    contains(normalizedName, "{") ||
+    contains(normalizedName, "}")
   ) {
     return { error: "Invalid characters in name." };
   }
 
-  if (!validator.matches(normalizedName, /^[a-zA-ZÀ-ÿ0-9\s.'-]+$/u)) {
+  if (!matches(normalizedName, /^[a-zA-ZÀ-ÿ0-9\s.'-]+$/u)) {
     return { error: "Name contains unsupported characters." };
   }
 
-  if (!email || typeof email !== "string" || !validator.isEmail(email)) {
+  if (!email || typeof email !== "string" || !isEmail(email)) {
     return { error: "Invalid email address." };
   }
 
-  const normalizedEmail: string | false = validator.normalizeEmail(email);
+  const normalizedEmail: string | false = normalizeEmail(email);
 
   if (!normalizedEmail) {
     return { error: "Invalid email format." };
